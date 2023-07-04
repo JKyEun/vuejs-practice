ed11148880df91d256f07747d0b74b6c2bdd29ca
JKyEun <j56237@naver.com>
2023-07-04 20:27:16 +0900

    data binding 및 메소드 사용한 DOM 컨트롤

--- src/views/MainHome.vue
+++ src/views/MainHome.vue
@@ -1,9 +1,27 @@
 <template lang="">
   <div>
-    <h1>Welcome to Home!</h1>
+    <h1>Welcome to {{ title }}!</h1>
+    <input type="text" v-model="inputValue" />
+    <button @click="getData">Get</button>
+    <button @click="setData">Set</button>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: 'HOME',
+      inputValue: 'Hi',
+    };
+  },
+  methods: {
+    getData() {
+      alert(this.inputValue);
+    },
+    setData() {
+      this.inputValue = 'Changed';
+    },
+  },
+};
 </script>
 <style lang=""></style>
