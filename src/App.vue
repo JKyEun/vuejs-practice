165b3839895b93055f8b91abb758a1c894f0f058
JKyEun <j56237@naver.com>
2023-07-04 19:32:05 +0900

    header 컴포넌트 생성 및 import

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainHeader />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from './components/layout/MainHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MainHeader,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/layout/MainHeader.vue
@@ -0,0 +1,22 @@
+<template lang="">
+  <div>
+    <ul class="header-ul">
+      <li>About</li>
+      <li>Buy</li>
+      <li>Sell</li>
+      <li>My Home</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.header-ul {
+  display: flex;
+  list-style: none;
+  width: 100%;
+  justify-content: space-around;
+  cursor: pointer;
+}
+</style>
