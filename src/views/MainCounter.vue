b003a7d042dbeed15d84a198bcc79bf82effb217
JKyEun <j56237@naver.com>
2023-07-22 19:44:03 +0900

    $ 문법 사용

--- src/views/MainCounter.vue
+++ src/views/MainCounter.vue
@@ -4,26 +4,28 @@
         <div>{{ count }}</div>
         <button @click="up">UP</button>
         <button @click="down">DOWN</button>
+        <button @click="go">GO HOME</button>
     </div>
 </template>
 
 <script>
-import store from '../store/index'
-
 export default {
     computed: {
         count() {
-            return store.state.count
+            return this.$store.state.count;
         }
     },
     methods: {
         up() {
-            store.commit('increment', { amount: 10 })
+            this.$store.commit('increment', { amount: 10 });
         },
         down() {
-            store.commit('decrement', { amount: 10 })
+            this.$store.commit('decrement', { amount: 10 });
+        },
+        push() {
+            this.$router.push('/');
         }
     }
-}
+};
 </script>
 <style lang=""></style>
